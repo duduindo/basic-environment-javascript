87d87e97ed919e1c75a5dc71136df68dfc32fd59
Eduardo Paixão <eduardo.paixao@grupofolha.com.br>
2018-06-07 14:57:08 -0300

    Add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
-const log = require('gulplog');
+const color = require('gulp-color');
 const babelify = require('babelify');
 const browserSync = require('browser-sync').create();
 
@@ -21,15 +21,21 @@ gulp.task('js', () => {
     .pipe(source('common.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify()).on('error', log.error)
+    .pipe(uglify()).on('error', e => console.error(e))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/js/'));
 
   return b;
 });
 
-
 gulp.task('watch', ['js'], () => {
+  gulp.watch('./src/js/**/*.js', ['js']);
+
+  gulp.watch(['./index.html', './dist/**/*.js']).on('change', e =>
+    console.info(color(`File: ${e.path}`, 'GREEN')));
+});
+
+gulp.task('server', ['js'], () => {
   browserSync.init({
     port: 3001,
     server: "./",
